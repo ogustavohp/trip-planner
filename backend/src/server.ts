eb41ad31bdee7b017d87c65540b6039781ee10c2
Gustavo Henrique <gustavohpereira1.6@gmail.com>
2024-07-11 17:56:38 -0300

    finished route `/trips` and create `/trips/:tripId/confirm`

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,24 @@
-console.log('a')
+import fastify from 'fastify'
+import { createTrip } from '@/routes/crete-trip'
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { confirmTrip } from '@/routes/confirm-trip'
+import cors from '@fastify/cors'
+
+const app = fastify()
+
+app.register(cors, {
+  origin: '*',
+})
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(createTrip)
+app.register(confirmTrip)
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('Server running!')
+})
